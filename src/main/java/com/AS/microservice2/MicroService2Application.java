cd0514fb372f5d82db231cbda8fe6214ec894301
ashishsharma <ashishsharma@192.168.1.5>
2017-07-09 21:21:36 +0530

    Actuators

---
+++ src/main/java/com/AS/microservice2/MicroService2Application.java
@@ -0,0 +1,12 @@
+package com.AS.microservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroService2Application.class, args);
+	}
+}
